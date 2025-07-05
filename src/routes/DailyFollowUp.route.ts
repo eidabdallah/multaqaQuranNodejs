69bcd38d1090a36816eb29b43ab5c0d3b2e5977a
eidabdallah <eidabdallah971@gmail.com>
2025-07-06 00:09:11 +0300

    create validation for dailayFollow

--- src/routes/DailyFollowUp.route.ts
+++ src/routes/DailyFollowUp.route.ts
@@ -5,17 +5,18 @@ import { endPoints } from './../utils/EndPoint';
 import { AuthMiddleware } from './../middleware/auth';
 import StudentDailyFollowUpController from './../controller/StudentDailyFollowUp/StudentDailyFollowUp.controller';
 import StudentDailyFollowUpService from './../services/StudentDailyFollowUp.service';
+import { DailyFollowUpValidation } from './../validation/dailayFollow.validation';
 
 const router = Router();
 const dailyFollowService = new StudentDailyFollowUpService();
 const dailyFollowController = new StudentDailyFollowUpController(dailyFollowService);
 
-router.post("/" , AuthMiddleware.authorize(endPoints.supervisorOnly) ,AsyncHandler.asyncHandler(dailyFollowController.createDailyFollowUp));
-router.patch("/:id" , AuthMiddleware.authorize(endPoints.supervisorOnly) ,AsyncHandler.asyncHandler(dailyFollowController.updateDailyFollowUp));
-router.delete("/:id" , AuthMiddleware.authorize(endPoints.supervisorOnly) ,AsyncHandler.asyncHandler(dailyFollowController.deleteDailyFollowUp));
+router.post("/" ,Validator.validate(DailyFollowUpValidation.createDailyFollowUpSchema) ,AuthMiddleware.authorize(endPoints.supervisorOnly) ,AsyncHandler.asyncHandler(dailyFollowController.createDailyFollowUp));
+router.patch("/:id" , Validator.validate(DailyFollowUpValidation.updateDailyFollowUpSchema) ,AuthMiddleware.authorize(endPoints.supervisorOnly) ,AsyncHandler.asyncHandler(dailyFollowController.updateDailyFollowUp));
+router.delete("/:id" ,Validator.validate(DailyFollowUpValidation.idDailyFollowUpSchema) , AuthMiddleware.authorize(endPoints.supervisorOnly) ,AsyncHandler.asyncHandler(dailyFollowController.deleteDailyFollowUp));
 router.get("/" , AuthMiddleware.authorize(endPoints.studentOnly) ,AsyncHandler.asyncHandler(dailyFollowController.getAllDailyFollowUpForStudent));
 // Supervisor views daily follow-up for a specific student by ID.
-router.get("/statistics" , AuthMiddleware.authorize(endPoints.adminOrDoctorOnly) ,AsyncHandler.asyncHandler(dailyFollowController.getStatistics));
-router.get("/:id" , AuthMiddleware.authorize(endPoints.supervisorOnly) ,AsyncHandler.asyncHandler(dailyFollowController.getAllDailyFollowUpForSuperVisor));
+router.get("/statistics" ,Validator.validate(DailyFollowUpValidation.statisticsSchema) , AuthMiddleware.authorize(endPoints.adminOrDoctorOnly) ,AsyncHandler.asyncHandler(dailyFollowController.getStatistics));
+router.get("/:id" ,Validator.validate(DailyFollowUpValidation.idDailyFollowUpSchema) , AuthMiddleware.authorize(endPoints.supervisorOnly) ,AsyncHandler.asyncHandler(dailyFollowController.getAllDailyFollowUpForSuperVisor));
 
 export default router;
---
+++ src/validation/dailayFollow.validation.ts
@@ -0,0 +1,74 @@
+import Joi from "joi";
+import { Validator } from './../middleware/validation';
+import { CollegesEnum } from "../utils/enum/college.enum.js";
+
+export class DailyFollowUpValidation {
+    static createDailyFollowUpSchema = Joi.object({
+        userId: Validator.generalFields.id,
+        savedInfo: Joi.string().required().messages({
+            "string.empty": "معلومات الحفظ مطلوبة.",
+            "any.required": "معلومات الحفظ مطلوبة.",
+        }),
+        ReviewInfo: Joi.string().required().messages({
+            "string.empty": "معلومات المراجعة مطلوبة.",
+            "any.required": "معلومات المراجعة مطلوبة.",
+        }),
+        note: Joi.string().required().messages({
+            "string.empty": "الملاحظة مطلوبة.",
+            "any.required": "الملاحظة مطلوبة.",
+        }),
+        pageNumberSaved: Joi.string().required().messages({
+            "string.empty": "رقم صفحة الحفظ مطلوب.",
+            "any.required": "رقم صفحة الحفظ مطلوب.",
+        }),
+        pageNumberReview: Joi.string().required().messages({
+            "string.empty": "رقم صفحة المراجعة مطلوب.",
+            "any.required": "رقم صفحة المراجعة مطلوب.",
+        }),
+        date: Joi.string().required().messages({
+            "string.empty": "التاريخ مطلوب.",
+            "any.required": "التاريخ مطلوب.",
+        })
+    })
+    static updateDailyFollowUpSchema = Joi.object({
+        savedInfo: Joi.string().messages({
+            "string.empty": "معلومات الحفظ مطلوبة.",
+        }),
+        ReviewInfo: Joi.string().messages({
+            "string.empty": "معلومات المراجعة مطلوبة.",
+        }),
+        note: Joi.string().messages({
+            "string.empty": "الملاحظة مطلوبة.",
+        }),
+        pageNumberSaved: Joi.string().messages({
+            "string.empty": "رقم صفحة الحفظ مطلوب.",
+        }),
+        pageNumberReview: Joi.string().messages({
+            "string.empty": "رقم صفحة المراجعة مطلوب.",
+        }),
+        date: Joi.string().messages({
+            "string.empty": "التاريخ مطلوب.",
+        })
+    })
+    static idDailyFollowUpSchema = Joi.object({
+        id: Validator.generalFields.id
+    })
+    static statisticsSchema = Joi.object({
+        semester: Joi.string().messages({
+            "string.base": "الفصل الدراسي يجب أن يكون نصًا.",
+        }),
+        fullName: Validator.generalFields.fullName,
+        college: Joi.string().valid(...Object.values(CollegesEnum)).messages({
+            "any.only": "الكلية غير صالحة.",
+        }),
+        halaqaName: Joi.string().messages({
+            "string.base": "اسم الحلقة يجب أن يكون نصًا.",
+
+        }),
+        gender: Joi.string().valid('Male', 'Female').required().messages({
+            "any.only": "الجنس يجب أن يكون 'ذكر' أو 'أنثى'.",
+            "any.required": "الجنس مطلوب.",
+        }),
+    })
+
+}
